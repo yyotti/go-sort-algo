d0fdbaba292359fb6bb8d9d8aca6e25c0c222f39
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2018-05-31 18:49:20 +0900

    Implement merge sort

---
+++ sort/merge.go
@@ -0,0 +1,52 @@
+package sort
+
+type mergeSorter struct {
+	src []Comparable
+}
+
+// Sort returns slice sorted by `Merge sort`
+// https://ja.wikipedia.org/wiki/%E3%83%9E%E3%83%BC%E3%82%B8%E3%82%BD%E3%83%BC%E3%83%88
+func (b mergeSorter) Sort() {
+	copy(b.src, mergeSort(b.src))
+}
+
+func mergeSort(src []Comparable) []Comparable {
+	l := len(src)
+	if l < 2 {
+		return src
+	}
+
+	half1 := mergeSort(src[:l/2])
+	half2 := mergeSort(src[l/2:])
+
+	dest := make([]Comparable, 0, l)
+	i, j := 0, 0
+	for i < len(half1) && j < len(half2) {
+		if half1[i].IsLessThan(half2[j]) {
+			dest = append(dest, half1[i])
+			i++
+		} else {
+			dest = append(dest, half2[j])
+			j++
+		}
+	}
+
+	for ; i < len(half1); i++ {
+		dest = append(dest, half1[i])
+	}
+	for ; j < len(half2); j++ {
+		dest = append(dest, half2[j])
+	}
+
+	return dest
+}
+
+// NewMergeSorter returns mergeSorter instance
+func NewMergeSorter(src []Comparable) Sorter {
+	s := make([]Comparable, 0, len(src))
+	copy(s, src)
+
+	return mergeSorter{
+		src: s,
+	}
+}
